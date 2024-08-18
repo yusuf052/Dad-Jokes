c4e141f78d4bca9f2893c7c6ad3d0fd5121485fa
yusuf052 <imranyusuf1@gmail.com>
2024-08-18 09:38:14 +0530

    Add new file

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokeBtn = document.getElementById('jokeBtn');
+const jokeEl = document.getElementById('joke');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+
+
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    };
+
+    const res = await fetch('https://icanhazdadjoke.com/', config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+
+        
+}
+
